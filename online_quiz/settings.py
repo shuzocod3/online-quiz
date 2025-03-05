a6ecd3103e8760a230a24cd22905059bad4a4e89
shuzo <152076317+shuzocod3@users.noreply.github.com>
2025-03-05 18:01:10 +0200

    1.1

--- online_quiz/settings.py
+++ online_quiz/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^$dsr&vyx^)zsilf&jhpk=)(+ukmi(lpv**29ubh7_zuxe9in+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,11 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "mediafiles"
 MEDIAFILES_DIRS = [
     BASE_DIR / "media",
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_URL = 'static/'
 

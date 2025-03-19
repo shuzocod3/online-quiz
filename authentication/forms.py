21a027a59d3b0187a0641baf73bb8f3c11e93740
shuzo <152076317+shuzocod3@users.noreply.github.com>
2025-03-19 19:17:10 +0200

    1.2

--- authentication/forms.py
+++ authentication/forms.py
@@ -5,7 +5,7 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'password1', 'password2', 'role']
+        fields = ['username', 'email', 'password1', 'password2']
 
 
 class UserProfileForm(forms.ModelForm):
--- authentication/models.py
+++ authentication/models.py
@@ -6,11 +6,5 @@ from django.db import models
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    ROLE_CHOICES = (
-        ('user', 'Користувач'),
-        ('moderator', 'Модератор'),
-        ('admin', 'Адміністратор'),
-    )
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='user')
     bio = models.TextField(null=True, blank=True)
     avatar = models.ImageField(upload_to='avatars', blank=True, null=True)
